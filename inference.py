957dc714f00a6e45def1e4916998fd92e04ada5b
Lukamanvelidze <148325545+Lukamanvelidze@users.noreply.github.com>
2025-06-06 18:41:07 +0200

    Add files via upload

--- inference.py
+++ inference.py
@@ -2,11 +2,34 @@ import cv2
 from ultralytics import YOLO
 
 model = YOLO('global_model.pt')
+model.model.names = {
+    0: "bike",
+    1: "bus",
+    2: "caravan",
+    3: "coupe",
+    4: "crossover",
+    5: "hatchback",
+    6: "jeep",
+    7: "mpv",
+    8: "pickup-truck",
+    9: "sedan",
+    10: "suv",
+    11: "taxi",
+    12: "truck",
+    13: "van",
+    14: "vehicle",
+    15: "wagon"
+}
+print(model.names)
+
+
 video_path = '/home/luka/Desktop/AIC_2023_Track2/AIC23_Track2_NL_Retrieval/data/validation/S02/c007/vdo.avi'
 
 # Set delay (milliseconds) for slowing down video
 # For example, 50 ms delay ≈ 20 fps playback speed
 delay = 50
+print(model.model.model[-1].nc)  # Should print 16
+
 
 try:
     results = model.predict(
@@ -19,9 +42,6 @@ try:
     )
 
     for result in results:
-        # result.show() is internally called with show=True, so frame is displayed
-
-        # Wait for 'delay' ms and check if 'q' is pressed to quit
         if cv2.waitKey(delay) & 0xFF == ord('q'):
             print("Quitting...")
             break
