e2ac17393910961836fe6b6d15be91b9cc71c533
Lukamanvelidze <148325545+Lukamanvelidze@users.noreply.github.com>
2025-06-27 16:00:21 +0200

    Update task.py

--- train/server/task.py
+++ train/server/task.py
@@ -52,7 +52,7 @@ def set_weights(net, parameters):
     model_keys = list(model_state.keys())
     params_dict = dict(zip(model_keys, parameters))
 
- prevent hard crash if there is any layer mismatch 
+# prevent hard crash if there is any layer mismatch 
     with torch.no_grad():  # Ensure we're not in inference mode
         for k, v in params_dict.items():
             try:
